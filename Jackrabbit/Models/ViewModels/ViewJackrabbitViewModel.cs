02d29785781614395f7ef6e3dc2140416e676ac6
Jordan Minx <jminx@engagesoftware.com>
2016-10-27 11:11:09 -0500

    Add LibraryViewModel

--- Jackrabbit/Models/ViewModels/ViewJackrabbitViewModel.cs
+++ Jackrabbit/Models/ViewModels/ViewJackrabbitViewModel.cs
@@ -11,8 +11,11 @@
 
 namespace Engage.Dnn.Jackrabbit
 {
+    using System;
     using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
+
+    using DotNetNuke.Framework.JavaScriptLibraries;
 
     /// <summary>The view model for the Jackrabbit, to be displayed by <see cref="IViewJackrabbitView"/></summary>
     public class ViewJackrabbitViewModel
@@ -25,6 +28,8 @@ namespace Engage.Dnn.Jackrabbit
 
         /// <summary>Gets or sets the files being included by this module.</summary>
         public IEnumerable<FileViewModel> Files { get; set; }
+
+        public IEnumerable<LibraryViewModel> Libraries { get; set; }
 
         /// <summary>Gets or sets the default path prefix.</summary>
         public string DefaultPathPrefix { get; set; }
@@ -81,6 +86,58 @@ namespace Engage.Dnn.Jackrabbit
 
             /// <summary>Gets the priority.</summary>
             public int Priority { get; }
+        }
+
+        public class LibraryViewModel
+        {
+            /// <summary>Initializes a new instance of the <see cref="LibraryViewModel" /> class.</summary>
+            /// <param name="fileType">Type of the file.</param>
+            /// <param name="id">The ID of the file.</param>
+            /// <param name="pathPrefixName">Name of the path prefix.</param>
+            /// <param name="filePath">The file path.</param>
+            /// <param name="fullFilePath">The file path combined with the prefix.</param>
+            /// <param name="provider">The provider.</param>
+            /// <param name="priority">The priority.</param>
+            public LibraryViewModel(FileType fileType, int id, string pathPrefixName, string filePath, string fullFilePath, string provider, int priority, string libraryName, Version version, SpecificVersion versionSpecificity)
+            {
+                this.FileType = fileType;
+                this.Id = id;
+                this.PathPrefixName = pathPrefixName;
+                this.FilePath = filePath;
+                this.FullFilePath = fullFilePath;
+                this.Provider = provider;
+                this.Priority = priority;
+                this.LibraryName = libraryName;
+                this.Version = version;
+                this.VersionSpecificity = versionSpecificity;
+            }
+
+            /// <summary>Gets the type of the file.</summary>
+            public FileType FileType { get; }
+
+            /// <summary>Gets the ID of the file.</summary>
+            public int Id { get; }
+
+            /// <summary>Gets the name of the path prefix.</summary>
+            public string PathPrefixName { get; }
+
+            /// <summary>Gets the file path.</summary>
+            public string FilePath { get; }
+
+            /// <summary>Gets the file path combined with the prefix.</summary>
+            public string FullFilePath { get; }
+
+            /// <summary>Gets the provider.</summary>
+            public string Provider { get; }
+
+            /// <summary>Gets the priority.</summary>
+            public int Priority { get; }
+
+            public string LibraryName { get; }
+
+            public Version Version { get; }
+
+            public SpecificVersion VersionSpecificity { get; }
         }
     }
 }
